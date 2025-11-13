92004c9431281bf258c7acc099e7351fe0afd153
Angel Gonzalez <angeldariogonzalezdomingez54@gmail.com>
2025-11-13 17:51:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -82,14 +82,14 @@ app.get('/api/auth/google', passport.authenticate('google', { scope: ['profile',
 app.get('/api/auth/google/callback',
     passport.authenticate('google', {
         // Pon tu URL de Netlify aquí
-        failureRedirect: 'https://TU-URL-DE-NETLIFY.netlify.app/login-cliente.html'
+        failureRedirect: 'https://hotel-oasis-u.netlify.app//login-cliente.html'
     }),
     (req, res) => {
         const payload = { id: req.user.id_cliente, nombre: req.user.nombre, apellido: req.user.apellido, email: req.user.email };
         const token = jwt.sign(payload, 'tu_llave_secreta_aqui', { expiresIn: '1h' });
 
         // ¡EL ARREGLO ESTÁ AQUÍ! Redirigimos al frontend con el token en la URL
-        res.redirect(`https://TU-URL-DE-NETLIFY.netlify.app/perfil.html?token=${token}`);
+        res.redirect(`https://hotel-oasis-u.netlify.app//perfil.html?token=${token}`);
     }
 );
 
